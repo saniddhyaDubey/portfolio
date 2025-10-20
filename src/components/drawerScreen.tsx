8b982ef9a5b0c8a22f20458b6aeace898e40c1d4
Saniddhya Dubey <dubeysaniddhya@gmail.com>
2025-10-20 18:34:01 -0400

    Updated Resume

--- src/components/drawerScreen.tsx
+++ src/components/drawerScreen.tsx
@@ -60,7 +60,7 @@ export default function SlidingDrawer() {
 
         <Link
           href={
-            "https://drive.google.com/file/d/1nJftLxrUkQcswfYD45kiNdjrsgkuL_Lj/view"
+            "https://drive.google.com/file/d/1ryH9HUG-xBzNET6gSTrfbi87Jz2IEBAk/view"
           }
           target="_blank"
           className="cursor-pointer font-courier absolute bottom-8 left-4 text-sm"
